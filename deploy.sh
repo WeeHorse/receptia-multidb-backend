b4d0dc8f71e98e1577ee272c6799ebf288e7329b
WeeHorse <benjaminberglund@gmail.com>
2022-01-23 23:55:45 +0100

    no restart

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # stop app
-/root/.nvm/versions/node/v16.13.2/bin/pm2 stop receptia
+# pm2 stop receptia
 
 # checkout release (wtf how hard..)
 git fetch && git reset --hard origin/release
 
 # restart app
-/root/.nvm/versions/node/v16.13.2/bin/pm2 restart receptia
+# pm2 restart receptia
